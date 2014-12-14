61042d5c046f212ff26992830c8d468045d14f68
Ky Meyer-Choi <ky_len@hotmail.com>
2014-12-13 20:38:48 -0800

    Migrate aanyang to js script. Install request.

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
+
+
+
+/*	***************************  */
+/*		Initialize annyang       */
+/*	***************************	 */
+
+var Annyang = require('annyang');
+var annyang = new Annyang();
+
+if (annyang) 
+{
+  var commands = {
+    'say hello' : function() 
+    {
+        console.log("Hello");
+    }
+  };
+
+  annyang.init(commands);
+
+}
 
 
 app.get('/', function (req, res) {
   res.render('index.ejs');
 })
 
+
+
+
+
+
+
+
+
+
+
+
+// Start the server
 var server = app.listen(3001, function () {
 
   var host = server.address().address
