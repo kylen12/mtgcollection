6538472a3140e8c18abc07f8dee9e0a1125267f2
Ky Meyer-Choi <ky_len@hotmail.com>
2014-12-15 13:05:10 -0800

    Update search card function to pull image url for cards

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// ../index.js
+
 var express = require('express');
 var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var db = require("./models");
+var passport = require("passport");
+var session = require("cookie-session");
 
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
@@ -17,17 +22,77 @@ var apiUrls =
 	showCardsInSet: "http://api.mtgapi.com/v2/sets?code="
 };
 
+app.use(session( {
+  secret: 'thisismysecretkey',
+  name: 'chocolate chip',
+  // this is in milliseconds
+  maxage: 3600000
+  })
+);
 
+// get passport started
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-app.get('/', function (req, res) {
-  res.render('index.ejs');
-})
-
-
-
-app.post('/cards/', function(req,res)
+passport.serializeUser(function(user, done) 
 {
+	console.log("Serialized");
+	done(null,user.id);
+});
+
+passport.deserializeUser(function(id, done) 
+{
+	console.log("Deserialized");
+
+	db.user.find ({ where: { id: id } })
+	.then(function(user) { done(null,user); },
+	function(err) { done(err,null); }
+	);
+});
+
+
+
+
+app.post('/login', function(req,res)
+{
+	// displays the login webpage
+	res.render('users/login');
+});
+
+
+app.post('/signup', function(req,res)
+{
+	// displays the sign up webpage
+	res.render('users/signup');
+});
+
+
+app.post('/authenticate/signup', function(req, res)
+{
+	// validate if information is legit
+
+	// create a new user
+
+	// redirect user to users/dashboard
+});
+
+
+app.post('/authenticate/login', function(req, res)
+{
+	// validate if input is acceptable
+
+	// checks if user exists
+		// if user does not exist, redirect to login page with msg
+		// if user exists, redirect to user/dashboard
+});
+
+
+app.post('/search', function(req,res)
+{
+	// pulls data from the api db based on search query
+
+	// redirects to cards/show with card list
+
 	var card = req.body.card;
 
 	request(apiUrls["showCards"]+card.name, function (error, response, body) 
@@ -35,11 +100,104 @@ app.post('/cards/', function(req,res)
 		if (!error && response.statusCode == 200) 
 		{
 			var obj = JSON.parse(body);
-			//console.log(obj);
+			console.log(obj[0].id);
 			
-			res.render("cards/index", { cardList : obj} );
+			request(apiUrls["showCardById"]+obj[0].id, function (error, response, body) 
+			{
+				if (!error && response.statusCode == 200) 
+				{
+					var obj = JSON.parse(body);
+
+		// console.log(obj.id);
+		// console.log(obj.name);
+		// console.log(obj.image);
+
+					console.log(obj[0].image);
+					//res.render("cards/cardList", { cardList : obj} );
+				}
+			});		
+
 		}
 	});		
+
+});
+
+
+
+
+
+app.post("/users", function (req, res) {
+  console.log("POST /users");
+  var newUser = req.body.user;
+  console.log("New User:", newUser);
+  db.user.createSecure(newUser.email, newUser.password, 
+    function () {
+      res.redirect("/signup");
+    },
+    function (err, user) {
+      req.login(user, function(){
+        console.log("Id: ", user.id)
+        res.redirect('/users/' + user.id);
+      });
+    }
+  )
+});
+
+// app.post('/login', passport.authenticate('local', {
+//   successRedirect: '/',
+//   failureRedirect: '/login'
+// }));
+
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+
+app.get("/users", function (req, res) {
+  console.log(req.user)
+  if (req.user) {
+    res.render("users/index", {user: req.user});
+  } else {
+    res.render("users/index", {user: false});
+  }
+});
+
+app.post('/news', function(req,res) 
+{
+  var article = req.body.article;
+
+  db.article.create 
+  ({ 
+    title: article.title, 
+    summary: article.summary, 
+    content: article.content, 
+    imgurl: article.imgurl    
+  })
+    .then(function(article) {
+    res.redirect("/news/" + article.id);
+  });  
+});
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', function (req, res) {
+  res.render('site/index.ejs');
+})
+
+
+
+app.post('/cards/', function(req,res)
+{
 });
 
 
