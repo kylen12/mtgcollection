3e1949a53f6e27534a05a5d4b36efdc90b9f2d3a
Ky Meyer-Choi <ky_len@hotmail.com>
2014-12-16 00:36:51 -0800

    Set up passport. Create login, sign up pages. Enable db insertion when signup form is submitted

--- index.js
+++ index.js
@@ -13,15 +13,13 @@ app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Set up an object that stores the different query urls
-var apiUrls =
-{
-	showCards : "http://api.mtgapi.com/v1/card/name/",
-	showCardById : "http://api.mtgapi.com/v1/card/id/",
-	showSets: "http://api.mtgapi.com/v2/sets",
-	showCardsInSet: "http://api.mtgapi.com/v2/sets?code="
-};
+/*
+  What is the session?
 
+  It is the object that lives in our app
+    and records relevant info about users
+    who are signed in
+*/
 app.use(session( {
   secret: 'thisismysecretkey',
   name: 'chocolate chip',
@@ -34,194 +32,103 @@ app.use(session( {
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) 
-{
-	console.log("Serialized");
-	done(null,user.id);
+/*
+SERIALizING
+Turns relevant user data into a string to be 
+  stored as a cookie
+*/
+passport.serializeUser(function(user, done){
+  console.log("SERIALIZED JUST RAN!");
+
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) 
-{
-	console.log("Deserialized");
+/*
+DeSERIALizing
 
-	db.user.find ({ where: { id: id } })
-	.then(function(user) { done(null,user); },
-	function(err) { done(err,null); }
-	);
-});
-
-
-
-
-app.post('/login', function(req,res)
-{
-	// displays the login webpage
-	res.render('users/login');
-});
-
-
-app.post('/signup', function(req,res)
-{
-	// displays the sign up webpage
-	res.render('users/signup');
-});
-
-
-app.post('/authenticate/signup', function(req, res)
-{
-	// validate if information is legit
-
-	// create a new user
-
-	// redirect user to users/dashboard
-});
-
-
-app.post('/authenticate/login', function(req, res)
-{
-	// validate if input is acceptable
-
-	// checks if user exists
-		// if user does not exist, redirect to login page with msg
-		// if user exists, redirect to user/dashboard
-});
-
-
-app.post('/search', function(req,res)
-{
-	// pulls data from the api db based on search query
-
-	// redirects to cards/show with card list
-
-	var card = req.body.card;
-
-	request(apiUrls["showCards"]+card.name, function (error, response, body) 
-	{
-		if (!error && response.statusCode == 200) 
-		{
-			var obj = JSON.parse(body);
-			console.log(obj[0].id);
-			
-			request(apiUrls["showCardById"]+obj[0].id, function (error, response, body) 
-			{
-				if (!error && response.statusCode == 200) 
-				{
-					var obj = JSON.parse(body);
-
-		// console.log(obj.id);
-		// console.log(obj.name);
-		// console.log(obj.image);
-
-					console.log(obj[0].image);
-					//res.render("cards/cardList", { cardList : obj} );
-				}
-			});		
-
-		}
-	});		
-
-});
-
-
-
-
-
-app.post("/users", function (req, res) {
-  console.log("POST /users");
-  var newUser = req.body.user;
-  console.log("New User:", newUser);
-  db.user.createSecure(newUser.email, newUser.password, 
-    function () {
-      res.redirect("/signup");
+Taking a string and turns into an object
+  using the relevant data stored in the session
+*/
+passport.deserializeUser(function(id, done){
+  console.log("DESERIALIZED JUST RAN!");
+  db.user.find({
+      where: {
+        id: id
+      }
+    })
+    .then(function(user){
+      done(null, user);
     },
-    function (err, user) {
-      req.login(user, function(){
-        console.log("Id: ", user.id)
-        res.redirect('/users/' + user.id);
-      });
-    }
-  )
-});
-
-// app.post('/login', passport.authenticate('local', {
-//   successRedirect: '/',
-//   failureRedirect: '/login'
-// }));
-
-
-app.get("/logout", function (req, res) {
-  req.logout();
-  res.redirect("/");
+    function(err) {
+      done(err, null);
+    });
 });
 
 
-app.get("/users", function (req, res) {
-  console.log(req.user)
+
+// Authenticating a user
+app.get("/", function (req, res) {
+  // req.user is the user currently logged in
+
   if (req.user) {
-    res.render("users/index", {user: req.user});
+    res.render("site/index", {user: req.user});
   } else {
-    res.render("users/index", {user: false});
+    res.render("site/index", {user: false});
   }
 });
 
-app.post('/news', function(req,res) 
-{
-  var article = req.body.article;
 
-  db.article.create 
-  ({ 
-    title: article.title, 
-    summary: article.summary, 
-    content: article.content, 
-    imgurl: article.imgurl    
-  })
-    .then(function(article) {
-    res.redirect("/news/" + article.id);
-  });  
+app.get("/users/:id", function (req, res) {
+  console.log("User created");
+  var id = req.params.id;
+  db.user.find(id)
+    .then(function (user) {
+      res.render("users/dashboard", {user: user});
+    })
+    .error(function () {
+      res.redirect("/users/signup");
+    })
 });
 
-
-
-
-
-
-
-
-
-
-
-app.get('/', function (req, res) {
-  res.render('site/index.ejs');
-})
-
-
-
-app.post('/cards/', function(req,res)
+// WHEN SOMEONE  SUBMITS A SIGNUP PAGE
+app.post("/signup", function (req, res) 
 {
+  console.log("POST /users");
+  var newUser = req.body.user;
+  console.log("New User:", newUser);
+  //CREATE a user and secure their password
+  db.user.createSecure(newUser.email, newUser.password, 
+    function () {
+      console.log("Redirecting");
+      // if a user fails to create make them signup again
+      res.redirect("/signup");
+    },
+    function (err, user) {
+      // when successfully created log the user in
+      // req.login is given by the passport
+      req.login(user, function(){
+        // after login redirect show page
+        console.log("Id: ", user.id)
+        res.redirect('/users/' + user.id);
+      });
+    })
 });
 
-
-
-
-app.get('/cards/:id', function(req, res)
-{
-	request(apiUrls["showCardById"]+req.params.id, function (error, response, body) 
-	{
-		if (!error && response.statusCode == 200) 
-		{
-			var obj = JSON.parse(body);
-			//console.log(obj);
-
-			res.render("cards/show", { card : obj[0]} );
-		}
-	});		
+// WHEN SOMEONE WANTS THE SIGNUP PAGE
+app.get("/signup", function (req, res) {
+  res.render("users/signup");
 });
 
+// When someone wants the login page
+app.get("/login", function (req, res) {
+  res.render("users/login");
+});
 
-
-
-
-
+app.get("/logout", function (req, res) {
+  // log out
+  req.logout();
+  res.redirect("/");
+});
 
 
 // Start the server
